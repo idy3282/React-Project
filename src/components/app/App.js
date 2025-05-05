28cd87e3c4a64796b07e82aa226d6901eecb4705
miri <miri@miri>
2025-05-05 16:42:30 +0300

    delete comands

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,22 +1,3 @@
-// import './App.css';
-// import {Routing} from '../Routing'
-// import { Route, Router, Routes, useNavigate } from 'react-router-dom';
-// import Layout from '../Layout';
-// import { Work } from '../works/work';
-
-
-// function App() {
-//   const navigate = useNavigate()
-//   return (
-//     <Router>
-//       <Layout>
-//       <Routing/>
-
-//       </Layout>
-//     </Router>
-//   );
-// }
-// export default App;
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Routing } from '../Routing';
